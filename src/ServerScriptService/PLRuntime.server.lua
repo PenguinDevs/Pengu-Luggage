c5a33a018c92f906163b1ee9b59a740704a12b3c
Jason Yi <Jasonyi1413@gmail.com>
2021-06-11 11:45:47 +1000

    Runtime Simplified

--- src/ServerScriptService/PLRuntime.server.lua
+++ src/ServerScriptService/PLRuntime.server.lua
@@ -1,1 +1,10 @@
+-- Responsible for initialising modules listed for server
+-- @author PenguinDevs
+
 local Resources = require(game.ReplicatedStorage.Resources)
+local ServerInitMods = Resources:LoadLibrary("ServerInitMods")
+local LoadPriorities = Resources:LoadLibrary("LoadPriorities")
+
+LoadPriorities(ServerInitMods)
+
+error("blah")
--- src/StarterPlayer/StarterPlayerScripts/PLRuntime.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/PLRuntime.client.lua
@@ -1,1 +1,8 @@
+-- Responsible for initialising modules listed for server
+-- @PenguinDevs
+
 local Resources = require(game.ReplicatedStorage.Resources)
+local ClientInitMods = Resources:LoadLibrary("ClientInitMods")
+local LoadPriorities = Resources:LoadLibrary("LoadPriorities")
+
+LoadPriorities(ClientInitMods)
