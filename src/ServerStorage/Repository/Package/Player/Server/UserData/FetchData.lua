e98f1706cf73f1296eef61051cc984e4999d2f1b
Jason Yi <Jasonyi1413@gmail.com>
2021-06-14 16:07:33 +1000

    Added FetchData for clients retrieving user data

--- src/ServerStorage/Repository/Package/Player/Server/UserData/FetchData.lua
+++ src/ServerStorage/Repository/Package/Player/Server/UserData/FetchData.lua
@@ -4,10 +4,24 @@
 local module = {}
 
 local Resources = require(game.ReplicatedStorage.Resources)
+local PlayerProfiles = Resources:LoadLibrary("PlayerProfiles")
+local DefaultDS = Resources:LoadLibrary("DefaultDS")
 
 function module:init()
     Resources:GetRemote("FetchData").OnServerInvoke = function(player, requestedData)
-        
+        local collectedReturn = {}
+
+        local PlayerProfile = PlayerProfiles:get(player)
+
+        for _, dataType in pairs(requestedData) do
+            if DefaultDS[dataType] then
+                collectedReturn[dataType] = PlayerProfile.data:get(dataType)
+            else
+                error(string.format("%s is not reigstered on DefaultDS module", dataType))
+            end
+        end
+
+        return collectedReturn
     end
 end
 
