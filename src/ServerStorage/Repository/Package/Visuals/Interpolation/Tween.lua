ea3b2e6d33684589163874c45468c40ae30f0cd1
Jason Yi <Jasonyi1413@gmail.com>
2021-06-14 20:09:25 +1000

    Initialised Tween

--- src/ServerStorage/Repository/Package/Visuals/Interpolation/Tween.lua
+++ src/ServerStorage/Repository/Package/Visuals/Interpolation/Tween.lua
@@ -1,180 +1,81 @@
--- Light-weight, Bezier-friendly Property Tweening
--- @documentation https://rostrap.github.io/Libraries/Interpolation/Tween/
--- @author Validark
+-- Tween service simplified in a module, as well as supporting overrides
+-- @author PenguinDevs
 
-local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Resources = require(game.ReplicatedStorage.Resources)
 
-local Resources = require(ReplicatedStorage:WaitForChild("Resources"))
-local Lerps = Resources:LoadLibrary("Lerps")
-local Table = Resources:LoadLibrary("Table")
-local Typer = Resources:LoadLibrary("Typer")
-local Signal = Resources:LoadLibrary("Signal")
-local Enumeration = Resources:LoadLibrary("Enumeration")
-local EasingFunctions = Resources:LoadLibrary("EasingFunctions")
+local TweenService = game:GetService("TweenService")
+local Janitor = Resources:LoadLibrary("Janitor")
 
-local Heartbeat = RunService.Heartbeat
-local RenderStepped = RunService.RenderStepped
+local module = {}
+local meta = {}
 
-local Completed = Enum.TweenStatus.Completed
-local Canceled = Enum.TweenStatus.Canceled
-local Linear = EasingFunctions[Enumeration.EasingFunction.Linear.Value]
+local storedTweens = {}
 
-local function GetRenderEvent(self)
-	local Object = self.Object
+function module.new(object, goals, tweenInfo, override)
+	local tween = TweenService:Create(object, tweenInfo, goals)
 
-	if typeof(Object) == "Instance" and Object.ClassName == "Camera" then
-		return RenderStepped
-	else
-		return Heartbeat
-	end
-end
+	local self = {}
+	self.tween = tween
+	self.janitor = Janitor.new()
 
-local Tween = {
-	__index = {
-		Running = false;
-		Duration = 1;
-		ElapsedTime = 0;
-		EasingFunction = EasingFunctions[Enumeration.EasingFunction.Standard.Value];
-		LerpFunction = function(StartValue, EndValue, Alpha) return EndValue end;
-	}
-}
+	function self:Play()
+		self.tween:Play()
 
-local OpenTweens = {} -- Will prevent objects from getting garbage collected until Tween finishes
-
-Tween.new = Typer.AssignSignature(Typer.OptionalNumber, Typer.OptionalFunctionOrEnumerationOfTypeEasingFunction, Typer.FunctionOrTableOrUserdata, Typer.Any, function(Duration, EasingFunction, Callback, Arg)
-	Duration = Duration or 1
-	EasingFunction = type(EasingFunction) == "userdata" and EasingFunctions[EasingFunction.Value] or EasingFunction or Linear
-
-	local self = setmetatable({
-		Duration = Duration;
-		Callback = Callback;
-		EasingFunction = EasingFunction;
-		Arg = Arg;
-	}, Tween)
-
-	function self.Interpolator(Step)
-		local ElapsedTime = self.ElapsedTime + Step
-		self.ElapsedTime = ElapsedTime
-
-		if Duration > ElapsedTime then
-			local x = EasingFunction(ElapsedTime, 0, 1, Duration)
-			if Arg ~= nil then
-				Callback(Arg, x)
-			else
-				Callback(x)
+		local ret = {}
+		function ret:await()
+			if self.tween.PlaybackState == Enum.PlaybackState.Playing then
+				self.tween.Completed:Wait()
 			end
-		else
-			if Arg ~= nil then
-				Callback(Arg, 1)
+		end
+
+		return ret
+	end
+
+	function self:Cancel()
+		self.tween:Cancel()
+	end
+
+	function self:Destroy()
+		self.janitor:Cleanup()
+	end
+
+	self = setmetatable(self, {
+		__call = function()
+			self:Play()
+		end;
+		__index = function(_, ...)
+			if ... == "Status" then
+				return self.tween.PlaybackState
+			elseif ... == "Completed" then
+				return self.tween.Completed
 			else
-				Callback(1)
+				return self[...]
 			end
-			self:Stop()
 		end
-	end
+	})
 
-	return self:Resume()
-end)
+	if override then
+		if storedTweens[object] then
+			storedTweens[object]:Destroy()
+		end
+	end
+	self()
 
-function Tween.__index:Stop(Finished)
-	if self.Running then
-		self.Connection:Disconnect()
-		self.Running = false
-		local ObjectTable = OpenTweens[self.Object]
-		if ObjectTable then
-			ObjectTable[self.Property] = nil -- This is for override checks
+	self.janitor:Add(function()
+		if self.Status == Enum.PlaybackState.Playing then
+			self:Cancel()
 		end
-	end
-	local Callback = self.FinishedCallback
-	if Callback == true then
-		if Finished then
-			self.Object:Destroy()
-		end
-	elseif Callback then
-		if self.CallbackArg ~= nil then
-			Callback(self.CallbackArg, Finished and Completed or Canceled)
-		else
-			Callback(Finished and Completed or Canceled)
-		end
-	end
+	end)
+	self.janitor:Add(self.tween, "Destroy")
+
+	storedTweens[object] = self
+	self.janitor:Add(function()
+		storedTweens[object] = nil
+	end)
+
 	return self
 end
 
-function Tween.__index:Resume()
-	if self.Duration == 0 then
-		self.Object[self.Property] = self.EndValue
-	else
-		if not self.Running then
-			self.Connection = GetRenderEvent(self):Connect(self.Interpolator)
-			self.Running = true
-			local ObjectTable = OpenTweens[self.Object]
-			if ObjectTable then
-				ObjectTable[self.Property] = self -- This is for override checks
-			end
-		end
-	end
-	return self
-end
+meta.__call = module.new
 
-function Tween.__index:Restart()
-	self.ElapsedTime = 0
-	return self:Resume()
-end
-
-function Tween.__index:Wait()
-	local Event = GetRenderEvent(self)
-	while self.Running do Event:Wait() end
-	return self
-end
-
-return Table.Lock(Tween, Typer.AssignSignature(5, Typer.OptionalFunctionOrEnumerationOfTypeEasingFunction, Typer.OptionalNumber, Typer.OptionalBoolean, Typer.OptionalFunctionOrTableOrUserdataOrTrue, Typer.Any, function(_, Object, Property, EndValue, EasingFunction, Duration, Override, Callback, CallbackArg)
-	Duration = Duration or 1
-	local LerpFunction = Lerps[typeof(EndValue)]
-	local StartValue = Object[Property]
-	EasingFunction = type(EasingFunction) == "userdata" and EasingFunctions[EasingFunction.Value] or EasingFunction or Linear
-
-	local self = setmetatable({
-		Duration = Duration;
-		StartValue = StartValue;
-		EndValue = EndValue;
-		LerpFunction = LerpFunction;
-		Object = Object;
-		FinishedCallback = Callback;
-		Property = Property;
-		EasingFunction = EasingFunction;
-		CallbackArg = CallbackArg;
-	}, Tween)
-
-	function self.Interpolator(Step)
-		local ElapsedTime = self.ElapsedTime + Step
-		self.ElapsedTime = ElapsedTime
-
-		if Duration > ElapsedTime then
-			Object[Property] = LerpFunction(StartValue, EndValue, EasingFunction(ElapsedTime, 0, 1, Duration))
-		else
-			self:Stop(true)
-			Object[Property] = EndValue
-		end
-	end
-
-	local ObjectTable = OpenTweens[Object] -- Handle Overriding Interpolations
-
-	if ObjectTable then
-		local OpenTween = ObjectTable[Property]
-		if OpenTween then
-			if Override then
-				OpenTween:Stop()
-			else
-				return self:Stop()
-			end
-		end
-	else
-		ObjectTable = {}
-		OpenTweens[Object] = ObjectTable
-	end
-
-	ObjectTable[Property] = self
-
-	return self:Resume()
-end))
+return setmetatable(module, meta)
